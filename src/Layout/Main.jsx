d4828fdc9e976ec29fe787dd3241ae06087139f8
rosol111 <gulamrosul037@gmail.com>
2023-10-29 00:22:30 +0600

    Added set  basic Layout for footer and header

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Sheard/Footer/Footer";
+import Navbar from "../Pages/Sheard/Navbar/Navbar";
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-import router from './Routes/Routes.jsx';
-import React from 'react';
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes.jsx";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-        <RouterProvider router={router} />
-
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+    ,
+  </div>
+);
