548dc8acd30a9566fb0e5047c9f7b693cf7dee31
rosol111 <gulamrosul037@gmail.com>
2023-11-01 03:48:46 +0600

    Added  insert bulk data in database and load servirs data

--- src/Pages/Home/Services/ServicesCard.jsx
+++ src/Pages/Home/Services/ServicesCard.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom";
+
+/* eslint-disable react/prop-types */
 const ServicesCard = ({ service }) => {
-    const { title, img, price } = service;
+  const { _id, title, img, price } = service;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
-        <img
-          src={img}
-          alt="image"
-          className="rounded-xl"
-        />
+        <img src={img} alt="image" className="rounded-xl" />
       </figure>
       <div className="card-body ">
-              <h2 className="card-title">{ title}</h2>
-              <p className="text-xl text-orange-500">Price: ${ price}</p>
-        
+        <h2 className="card-title">{title}</h2>
+        <p className="text-xl text-orange-500">Price: ${price}</p>
+        <div className="card-actions">
+          <Link to={`/checkout/${_id}`}>
+            <button className="btn btn-primary">Book Now</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
