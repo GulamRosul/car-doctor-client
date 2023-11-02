911ec94e01ab4a3ff906e51dea0d45138cc211cc
rosol111 <gulamrosul037@gmail.com>
2023-11-03 00:43:14 +0600

    Added Use query parameter to load user specific data

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,8 +3,9 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import SingUp from "../Pages/SingUp/SingUp";
-import Checkout from "../Pages/Checkout/Checkout";
+
 import BookService from "../Pages/BookService/BookService";
+import Bookings from "../Pages/Bookings/Bookings";
 
 const router = createBrowserRouter([
   {
@@ -30,10 +31,8 @@ const router = createBrowserRouter([
           fetch(`http://localhost:5000/services/${params.id}`),
       },
       {
-        path: "checkout/:id",
-        element: <Checkout></Checkout>,
-        loader: ({ params }) =>
-          fetch(`http://localhost:5000/services/${params.id}`),
+          path: 'bookings',
+          element: <Bookings></Bookings>
       },
     ],
   },
